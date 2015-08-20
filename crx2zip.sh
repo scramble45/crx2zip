d6a31537bcfafd0d3a9c917bde8ee5e1927cd15d
Rowan Hamilton <scramble@gmail.com>
2015-08-20 00:04:06 -0500

    upload script commit

---
+++ crx2zip.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Convert Chromium .CRX file to .ZIP, v1.0
+# Rowan H - 8/19/2015
+
+# This is a simple one shot bash script that uses: hexdump, sed and dd to
+# create a .ZIP archive from a .CRX file
+# my implementation is far from perfect and its possible if PK is
+# referenced sooner in the header the file header than its suppose to be
+# the creation using *dd* may fail.
+
+
+function checkDep-HexDump(){
+if [ $(dpkg-query -W -f='${Status}' bsdmainutils 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	echo 'It appears you are missing *hexdump* which is required. (Please compile it or install via apt-get)'
+  	sudo apt-get install bsdmainutils;
+fi
+}
+
+function checkDep-sed()
+if [ $(dpkg-query -W -f='${Status}' sed 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	echo 'It appears you are missing *sed* which is required. (Please compile it or install via apt-get)'
+  	sudo apt-get install sed;
+fi
+
+function checkDep-dd()
+if [ $(dpkg-query -W -f='${Status}' coreutils 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	echo 'It appears you are missing *dd* which is required. (Please compile it or install via apt-get)'
+  	sudo apt-get install coreutils;
+fi
+
+
+echo 'Checking for required packages for script to work...'
+checkDep-HexDump
+checkDep-sed
+
+
+reset
+
+RED='\033[0;31m'
+echo
+printf "${RED}CRX 2 ZIP\n"
+echo
+echo
+echo
+read -p "Enter the .CRX file path then press (Enter): " -i "$PWD/" -e crxPath
+echo
+
+getPK=$(hexdump -C $crxPath | grep 'PK' | sed -e 's/^\(.\{8\}\).*/\1/' | sed -n '1p')
+echo "PK-Address found at: $getPK"
+
+# Passes address to 'dd' to handle the rest
+
+zipContainerName=$(echo ${crxPath::-4})
+
+echo "Exporting our patched ZIP file to '.' "
+
+dd if=$crxPath of=$zipContainerName.zip bs=1 skip=$getPK
+echo
+echo
+echo 'You can now decompress your ZIP archive...'
+exit 1
